b98efa15de93fd878e6388b47d2e795fa8da991f
GeraldRyan <gerald.ryan40@yahoo.com>
2020-06-01 16:03:37 -0500

    got the args done

--- src/11_args.py
+++ src/11_args.py
@@ -6,13 +6,19 @@
 
 # YOUR CODE HERE
 
+def f1(a,b):
+    return a+b
+
 print(f1(1, 2))
+
 
 # Write a function f2 that takes any number of integer arguments and returns the
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
-
 # YOUR CODE HERE
+def f2(*argv):
+    return sum(argv)
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -22,7 +28,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,9 +36,14 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(a,b=0):
+    if b==0:
+        return a+1
+    else:
+        return a+b
 
-print(f3(1, 2))  # Should print 3
-print(f3(8))     # Should print 9
+print("f3",f3(1, 2))  # Should print 3
+print("f3",f3(8))     # Should print 9
 
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
@@ -44,6 +55,10 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+
+def f4(**kwargs):
+    for arg in kwargs:
+        print("Key:", arg, ", value: ", kwargs[arg], sep="")
 
 # Should print
 # key: a, value: 12
@@ -62,4 +77,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
