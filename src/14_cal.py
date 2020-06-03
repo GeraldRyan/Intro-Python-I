f0f23b23bab667a721cdbc7c76692cb97cbd725f
GeraldRyan <gerald.ryan40@yahoo.com>
2020-06-03 12:08:54 -0500

    getting calendear working

--- src/14_cal.py
+++ src/14_cal.py
@@ -18,16 +18,34 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
 import sys
-
-import calendar
 from datetime import datetime
+import calendar
+
+num_args = len(sys.argv)
+
+if num_args == 1:
+  month = datetime.now().month
+  year = datetime.now().year
+elif num_args == 2:
+  month = sys.argv[1]
+  year = datetime.now().year
+elif num_args ==3:
+  month = sys.argv[1]
+  year = sys.argv[2]
+else: 
+  print("user error. Please pass in month and year (2 arguments")
+
+try:
+  print(month, year)
+except:
+  print("Please enter month and year")
